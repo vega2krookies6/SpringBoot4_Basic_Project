e97f0f1908d4144097408a2b2fb9a62b1debf34c
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-31 14:15:56 +0900

    SpringApplication update

--- src/main/java/com/rookies6/myspringboot4project/MySpringBoot4ProjectApplication.java
+++ src/main/java/com/rookies6/myspringboot4project/MySpringBoot4ProjectApplication.java
@@ -8,8 +8,11 @@ import org.springframework.context.annotation.Bean;
 public class MySpringBoot4ProjectApplication {
 
 	public static void main(String[] args) {
-        SpringApplication.run(MySpringBoot4ProjectApplication.class, args);
-	}
+        //SpringApplication.run(MySpringBoot4ProjectApplication.class, args);
+        SpringApplication application = new SpringApplication(MySpringBoot4ProjectApplication.class);
+
+        application.run(args);
+    }
 
     @Bean
     public String myBean() {
