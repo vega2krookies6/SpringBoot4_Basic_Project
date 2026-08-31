326e91744a9e9a4ad27aaf44897ddc567614ced1
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-31 14:41:24 +0900

    MyRunner update

--- src/main/java/com/rookies6/myspringboot4project/runner/MyRunner.java
+++ src/main/java/com/rookies6/myspringboot4project/runner/MyRunner.java
@@ -1,11 +1,15 @@
 package com.rookies6.myspringboot4project.runner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements ApplicationRunner {
+    @Value("${spring.application.name}")
+    String applicationName;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("MyRunner run() 호출됨!!");
