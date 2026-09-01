d7ae543a5ecfc5e6542e1cc72d9d965cccbf38b9
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-01 14:48:17 +0900

    customer entity add

--- src/main/java/com/rookies6/myspringboot4project/entity/Customer.java
+++ src/main/java/com/rookies6/myspringboot4project/entity/Customer.java
@@ -1,12 +1,20 @@
 package com.rookies6.myspringboot4project.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "customers")
+@Getter @Setter
 public class Customer {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String customerId;
+
+    @Column(nullable = false)
+    private String customerName;
 
 }
