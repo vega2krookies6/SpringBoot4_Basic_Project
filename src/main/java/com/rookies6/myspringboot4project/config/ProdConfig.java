342ed7bac1f6d0f869ad13142a6479f51eefc750
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-01 10:22:02 +0900

    ProdConfig add

---
+++ src/main/java/com/rookies6/myspringboot4project/config/ProdConfig.java
@@ -0,0 +1,18 @@
+package com.rookies6.myspringboot4project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+//@Profile("prod")
+public class ProdConfig {
+    @Bean
+    public CustomVO customVO() {
+        return CustomVO.builder()
+                .mode("운영 환경")
+                .rate(1.5)
+                .build();
+    }
+
+}
