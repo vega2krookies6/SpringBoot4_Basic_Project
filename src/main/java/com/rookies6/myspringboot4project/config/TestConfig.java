9b2f4d560f30e7036bc8b051d4728f23b3f12229
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-01 10:16:57 +0900

    TestConfig add

---
+++ src/main/java/com/rookies6/myspringboot4project/config/TestConfig.java
@@ -0,0 +1,17 @@
+package com.rookies6.myspringboot4project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+//@Profile("test")
+public class TestConfig {
+    @Bean
+    public CustomVO customerVO() {
+        return CustomVO.builder() //CustomerVOBuilder
+                .mode("테스트 환경")
+                .rate(0.5)
+                .build();
+    }
+}
