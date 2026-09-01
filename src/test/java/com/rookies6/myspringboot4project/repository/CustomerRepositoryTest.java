5c7c4cbd96a02eec49191e19241fb8d176ef3185
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-01 16:55:55 +0900

    CustomerRepositoryTest update

--- src/test/java/com/rookies6/myspringboot4project/repository/CustomerRepositoryTest.java
+++ src/test/java/com/rookies6/myspringboot4project/repository/CustomerRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.rookies6.myspringboot4project.repository;
 
+import com.rookies6.myspringboot4project.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CustomerRepositoryTest {
@@ -13,6 +15,13 @@ class CustomerRepositoryTest {
 
     @Test
     void testCreate() {
+        //Given(준비단계)
+        Customer customer = new Customer();
+        customer.setCustomerId("A004");
+        customer.setCustomerName("둘리");
+        //When(실행단계)
+
+        //Then(검증단계)
 
     }
 }
