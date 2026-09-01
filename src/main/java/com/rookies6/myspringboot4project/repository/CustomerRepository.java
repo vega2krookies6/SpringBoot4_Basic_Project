eef073655c815c8be4b6bf07d829b5a7a7a4e82e
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-01 16:43:02 +0900

    CustomerRepository finder add

--- src/main/java/com/rookies6/myspringboot4project/repository/CustomerRepository.java
+++ src/main/java/com/rookies6/myspringboot4project/repository/CustomerRepository.java
@@ -3,11 +3,16 @@ package com.rookies6.myspringboot4project.repository;
 import com.rookies6.myspringboot4project.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
     //finder(query) method
     //1. customerId(고객번호, Unique이므로 중복허용않함)로 조회하는 finder 메서드
     Optional<Customer> findByCustomerId(String customerId);
+
+    //2. customerName(고객명, 중복허용함)로 조회하는 finder 메서드 % name %
+    List<Customer> findByCustomerNameContains(String customerName);
+
 
 }
