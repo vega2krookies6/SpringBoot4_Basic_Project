4f6159c95e179457c1792c74807e6197fe2359ce
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-01 16:50:27 +0900

    CustomerRepositoryTest update

--- src/test/java/com/rookies6/myspringboot4project/repository/CustomerRepositoryTest.java
+++ src/test/java/com/rookies6/myspringboot4project/repository/CustomerRepositoryTest.java
@@ -1,10 +1,18 @@
 package com.rookies6.myspringboot4project.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository customerRepository;
 
+    @Test
+    void testCreate() {
+
+    }
 }
