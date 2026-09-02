d5bb43555e02efdf5e858ea8a1652c2e3ffcfcbd
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-02 10:51:38 +0900

    repo test update

--- src/test/java/com/rookies6/myspringboot4project/repository/CustomerRepositoryTest.java
+++ src/test/java/com/rookies6/myspringboot4project/repository/CustomerRepositoryTest.java
@@ -20,13 +20,13 @@ class CustomerRepositoryTest {
     void testCreate() {
         //Given(준비단계)
         Customer customer = new Customer();
-        customer.setCustomerId("A004");
-        customer.setCustomerName("둘리");
+        customer.setCustomerId("B001");
+        customer.setCustomerName("길동");
         //When(실행단계)
         Customer addCustomer = customerRepository.save(customer);
         //Then(검증단계)
         assertThat(addCustomer).isNotNull();
-        assertThat(addCustomer.getCustomerName()).isEqualTo("둘리");
+        assertThat(addCustomer.getCustomerName()).isEqualTo("길동");
     }
 
     //2. Customer 조회
@@ -44,10 +44,16 @@ class CustomerRepositoryTest {
     @Test
     void testFindByNotFound() {
         //Optional의 orElseGet(Supplier) Supplier의 추상메서드 T get()  () -> T
-        // orElseThrow(Supplier) 사용 X get()  () -> X  ==> X extends Throwable
-        Customer notFoundCustomer = customerRepository.findByCustomerId("A004")//Optional<Customer>
+        Customer notFoundCustomer = customerRepository.findByCustomerId("B001")//Optional<Customer>
                 .orElseGet(() -> new Customer());
-        assertThat(notFoundCustomer.getCustomerId()).isEqualTo("A004");
+        //assertThat(notFoundCustomer.getCustomerId()).isEqualTo("A004");
+        assertThat(notFoundCustomer.getCustomerId()).isNull();
+
+        // orElseThrow(Supplier) 사용 X get()  () -> X  ==> X extends Throwable
+        //public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier)
+        Customer notFound = customerRepository.findById(3L)
+                .orElseThrow(() -> new RuntimeException("Customer Not Found"));
+
 
     }
 }
