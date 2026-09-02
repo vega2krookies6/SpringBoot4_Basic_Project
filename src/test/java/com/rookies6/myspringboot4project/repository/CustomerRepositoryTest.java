bbbf667de820e13f07cd4e1d0a1a64ead7041847
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-02 11:32:16 +0900

    repo test update

--- src/test/java/com/rookies6/myspringboot4project/repository/CustomerRepositoryTest.java
+++ src/test/java/com/rookies6/myspringboot4project/repository/CustomerRepositoryTest.java
@@ -4,6 +4,8 @@ import com.rookies6.myspringboot4project.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -11,22 +13,24 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
 
     //1. Customer 등록
     @Test
+    @Rollback(value = false) //등록된 데이터를 확인하려고 하니 Rollback 처리를 하지 마세요.
     void testCreate() {
         //Given(준비단계)
         Customer customer = new Customer();
-        customer.setCustomerId("B001");
-        customer.setCustomerName("길동");
+        customer.setCustomerId("D002");
+        customer.setCustomerName("길동3");
         //When(실행단계)
         Customer addCustomer = customerRepository.save(customer);
         //Then(검증단계)
         assertThat(addCustomer).isNotNull();
-        assertThat(addCustomer.getCustomerName()).isEqualTo("길동");
+        assertThat(addCustomer.getCustomerName()).isEqualTo("길동3");
     }
 
     //2. Customer 조회
@@ -53,7 +57,12 @@ class CustomerRepositoryTest {
         //public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier)
         Customer notFound = customerRepository.findById(3L)
                 .orElseThrow(() -> new RuntimeException("Customer Not Found"));
+    }
 
-
+    @Test
+    //@Rollback(value = false)
+    void testUpdate() {
+        Customer customer = customerRepository.findByCustomerId("A004")
+                .orElseGet(() -> new Customer());
     }
 }
