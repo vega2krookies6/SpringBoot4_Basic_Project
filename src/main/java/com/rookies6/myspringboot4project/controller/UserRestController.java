e12c0d48ea4e63748debafea3ac2fd18bce76692
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-02 15:33:08 +0900

    UserRestController update

--- src/main/java/com/rookies6/myspringboot4project/controller/UserRestController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserRestController.java
@@ -1,7 +1,19 @@
 package com.rookies6.myspringboot4project.controller;
 
+import com.rookies6.myspringboot4project.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
+@RequestMapping("/api/users")
 public class UserRestController {
+    private UserRepository userRepository;
+
+    //Constructor Injection
+    public UserRestController(UserRepository userRepository) {
+        log.info("UserRepository 구현 클래스명 = {}", userRepository.getClass().getName());
+        this.userRepository = userRepository;
+    }
 }
