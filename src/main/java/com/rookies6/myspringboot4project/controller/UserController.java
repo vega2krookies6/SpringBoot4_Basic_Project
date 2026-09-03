ba768838e29375e74448a7b2169aeffea481ab78
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-03 13:35:55 +0900

    UserController add

--- src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
@@ -1,14 +1,27 @@
 package com.rookies6.myspringboot4project.controller;
 
+import com.rookies6.myspringboot4project.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository userRepository;
+
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
         model.addAttribute("name","스프링부트");
         return "leaf";
     }
+
+    @GetMapping("/index")
+    public ModelAndView userList() {
+
+    }
+
+
 }
