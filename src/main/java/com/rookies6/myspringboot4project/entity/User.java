14ac40a6d6ef50b05b72ccebb25eab0653393007
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-03 15:31:58 +0900

    add-user controller add

--- src/main/java/com/rookies6/myspringboot4project/entity/User.java
+++ src/main/java/com/rookies6/myspringboot4project/entity/User.java
@@ -1,8 +1,10 @@
 package com.rookies6.myspringboot4project.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -17,9 +19,11 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
+    @NotEmpty
     private String name;
 
     @Column(unique = true, nullable = false)
+    @NotBlank(message = "Email 주소는 필수 입력 항목입니다.")
     private String email;
 
     @Column(nullable = false, updatable = false)
