6509c7728aa024ccaa52bd87c813630ca76d66ac
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-03 10:31:59 +0900

    controller deleteUser add

--- src/main/java/com/rookies6/myspringboot4project/controller/UserRestController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserRestController.java
@@ -7,6 +7,7 @@ import com.rookies6.myspringboot4project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -64,4 +65,8 @@ public class UserRestController {
         return userRepository.save(existUser);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id){
+
+    }
 }
