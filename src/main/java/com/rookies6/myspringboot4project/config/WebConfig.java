1bb2d1077d668584fd66b5595244129f54947e8d
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-03 11:01:41 +0900

    WebConfig add

--- src/main/java/com/rookies6/myspringboot4project/config/WebConfig.java
+++ src/main/java/com/rookies6/myspringboot4project/config/WebConfig.java
@@ -6,5 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+    }
 }
