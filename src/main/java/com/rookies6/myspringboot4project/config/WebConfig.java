3377cd5c16a5d58ed7cf3bfd1f948e469692e3c3
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-03 11:07:45 +0900

    WebConfig add

--- src/main/java/com/rookies6/myspringboot4project/config/WebConfig.java
+++ src/main/java/com/rookies6/myspringboot4project/config/WebConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
+        registry.addResourceHandler("/mobile/**")
+                //반드시 mobile 다음에 / 을 주어야 한다.
+                .addResourceLocations("classpath:/mobile/")
+                .setCachePeriod(20);//20초
     }
 }
