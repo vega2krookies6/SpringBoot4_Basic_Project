a33f0a9b9b1c574d41da9bf862f4ddc614d7f507
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-03 14:52:35 +0900

    add-user controller add

--- src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -30,7 +31,7 @@ public class UserController {
     }
 
     @GetMapping("/signup")
-    public String showSignUpForm() {
+    public String showSignUpForm(@ModelAttribute("userForm") User user) {
         return "add-user";
     }
 
