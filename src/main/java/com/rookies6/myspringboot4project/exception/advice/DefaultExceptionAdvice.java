250fa4aff9ec6e9b60229586929ad0d0b8582d41
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-04 16:54:19 +0900

    Advice update

--- src/main/java/com/rookies6/myspringboot4project/exception/advice/DefaultExceptionAdvice.java
+++ src/main/java/com/rookies6/myspringboot4project/exception/advice/DefaultExceptionAdvice.java
@@ -1,16 +1,22 @@
 package com.rookies6.myspringboot4project.exception.advice;
 
 import com.rookies6.myspringboot4project.exception.BusinessException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -63,4 +69,42 @@ public class DefaultExceptionAdvice {
 
         return new ResponseEntity<ErrorObject>(errorObject, HttpStatusCode.valueOf(500));
     }
-}
+
+    //입력항목 검증오류가 발생할때 동작하는 메서드
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+
+        log.error(ex.getMessage(), ex);
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getAllErrors()
+                .forEach((error) -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+
+        ValidationErrorResponse response =
+                new ValidationErrorResponse(
+                        400,
+                        "입력항목 검증 오류",
+                        LocalDateTime.now(),
+                        errors
+                );
+        //badRequest() 400
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class ValidationErrorResponse {
+        private int status;
+        private String message;
+        private LocalDateTime timestamp;
+        private Map<String, String> errors;
+    }//ValidationErrorResponse
+
+}//class
