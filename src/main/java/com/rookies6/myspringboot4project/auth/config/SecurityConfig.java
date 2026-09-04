10f3468dc713f75f7fa2ec156d922069e9354eea
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-04 14:44:53 +0900

    UserInfoUserDetailsãService add

--- src/main/java/com/rookies6/myspringboot4project/auth/config/SecurityConfig.java
+++ src/main/java/com/rookies6/myspringboot4project/auth/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/users/welcome").permitAll()
+                    auth.requestMatchers("/api/users/welcome","/userinfos/new").permitAll()
                             .requestMatchers("/api/users/**").authenticated();
                 })
                 .formLogin(withDefaults())
