c1cfe97685a416e68255eafc7557acb4318616b7
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-07 10:36:44 +0900

    StreamTest add

--- src/test/java/com/rookies6/myspringboot4project/StreamTest.java
+++ src/test/java/com/rookies6/myspringboot4project/StreamTest.java
@@ -18,13 +18,18 @@ public class StreamTest {
                 result1.add(name.toUpperCase());
             }
         }
+        System.out.println(result1);
         // 결과 : [ALICE, CHARLIE, DIANA]
 
         // (2) Stream 방식 : (1)과 완전히 같은 동작
-        List<String> result2 = names.stream()      	// ① 스트림 생성
+        List<String> result2 = names.stream()      	// ① 스트림 생성 List<String> => Stream<String>
+                //.filter(Predicate) Predicate 추상메서드 T -> boolean
                 .filter(name -> name.length() > 3) 	// ② 중간 연산 - 길이가 3 초과인 것만
-                .map(String::toUpperCase)          	// ② 중간 연산 - 대문자로 변환
-                .toList();                         	// ③ 최종 연산 - List 로 수집
+                //.map(Function) Function 추상메서드 T -> R
+                .map(name -> name.toUpperCase())
+                //.map(String::toUpperCase)          	// ② 중간 연산 - 대문자로 변환
+                .toList();                         	// ③ 최종 연산 - List 로 수집 Stream<String> => List<String>
+        System.out.println(result2);
         // 결과 : [ALICE, CHARLIE, DIANA]
 
     }
