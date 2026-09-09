d2253abab31e804a63c401bab50c0703ee2f199a
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-09 10:25:02 +0900

    dto update

--- src/main/java/com/rookies6/myspringboot4project/controller/dto/StudentDTO.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/dto/StudentDTO.java
@@ -37,15 +37,16 @@ public class StudentDTO {
     @AllArgsConstructor
     @Builder
     public static class StudentDetailDTO {
-        @NotBlank(message = "Address is required")
-        @Size(max = 200, message = "Address cannot exceed 200 characters")
+//        @NotBlank(message = "Address is required")
+//        @Size(max = 200, message = "Address cannot exceed 200 characters")
         private String address;
 
         @NotBlank(message = "Phone number is required")
         @Size(max = 20, message = "Phone number cannot exceed 20 characters")
         private String phoneNumber;
 
-        @Size(max = 100, message = "Email cannot exceed 100 characters")
+        @NotBlank(message = "Email is required")
+        @Size(max = 20, message = "Email cannot exceed 20 characters")
         @Email
         private String email;
 
