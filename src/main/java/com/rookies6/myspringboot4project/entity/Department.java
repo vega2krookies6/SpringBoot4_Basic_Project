370b0575868421eee45a94d85a23bd9eb8a6bbaf
vega2krookies6 <vega2krookies6@gmail.com>
2026-09-10 15:42:59 +0900

    department add

---
+++ src/main/java/com/rookies6/myspringboot4project/entity/Department.java
@@ -0,0 +1,38 @@
+package com.rookies6.myspringboot4project.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Department {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_id")
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    //양방향에서 Department에서 Student를 참조할 수 있도록 FK에 해당하는 필드명 mappedBy에 설정한다.
+    @OneToMany(
+            mappedBy = "department",
+            //cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    //빌더패턴을 적용했을때 변수에 명시적으로 초기화 한 값이 유지 되도록 해주는 어노테이션
+    @Builder.Default
+    private List<Student> students = new ArrayList<>();
+}
